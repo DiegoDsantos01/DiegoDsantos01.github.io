681bedaa7f2d85e21d306b79ea91854a1fb9f728
Diego <diegoaugustods1993@gmail.com>
2023-05-15 21:55:34 -0300

    Site beta

---
+++ script.js
@@ -0,0 +1,95 @@
+// Variavel para troca de texto da header
+var typed = new Typed(".auto-typed", {
+    strings: ["Desenvolvedor", "Ilustrador", "Artista 3D"],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true 
+})
+
+// // Variavel para o carrossel
+// const carouselItems = document.querySelectorAll(".carousel-item");
+// carouselItems.forEach(item => {
+//   item.addEventListener("click", () => {
+//     const imageUrl = item.getAttribute("src");
+//     const modal = document.createElement("div");
+//     modal.classList.add("modal");
+//     const modalContent = `
+//       <img src="${imageUrl}" alt="">
+//       <span class="close">&times;</span>
+//     `;
+//     modal.innerHTML = modalContent;
+//     document.body.appendChild(modal);
+//     const closeModal = () => {
+//       document.body.removeChild(modal);
+//     };
+//     const closeButton = modal.querySelector(".close");
+//     closeButton.addEventListener("click", closeModal);
+//     modal.addEventListener("click", closeModal);
+//   });
+// });
+const carousel = document.querySelector(".carousel");
+const carouselInner = document.querySelector(".carousel-inner");
+const modal = document.querySelector(".modal");
+
+const images = [
+  "img/projetos/sonic_copo.jpg",
+  "img/projetos/sonic_31.jpg",
+  "img/projetos/peridot.jpg",
+  "img/projetos/jadeGhost.jpg",
+  "img/projetos/eevee_suco.jpg",
+  "img/projetos/senseis.jpg",
+  "img/projetos/Puff.png",
+  "img/projetos/Vi.jpg",
+  "img/projetos/Zhongli.png",
+  "img/projetos/squirtle_niver.jpg",
+  "img/projetos/squirtle.png",
+  "img/projetos/klee_frente.jpg",
+  "img/projetos/zhong.png",
+  "img/projetos/manopla_vi_c.jpg",
+  "img/projetos/manopla_vi_f.jpg",
+];
+
+// Adiciona as imagens ao carrossel
+images.forEach((imageUrl) => {
+  const carouselItem = document.createElement("img");
+  carouselItem.classList.add("carousel-item");
+  carouselItem.setAttribute("src", imageUrl);
+  carouselInner.appendChild(carouselItem);
+});
+
+// Clona as imagens para criar uma rotação infinita
+const carouselItems = carouselInner.querySelectorAll(".carousel-item");
+carouselItems.forEach((item) => {
+  const clone = item.cloneNode(true);
+  carouselInner.appendChild(clone);
+});
+
+// Move as imagens para a esquerda para criar o efeito de rotação
+let position = 0;
+const moveCarousel = () => {
+  position--;
+  carouselInner.style.transform = `translateX(${position}px)`;
+  requestAnimationFrame(moveCarousel);
+};
+requestAnimationFrame(moveCarousel);
+
+// Abre o lightbox quando uma imagem é clicada
+carouselInner.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.classList.contains("carousel-item")) {
+    const imageUrl = target.getAttribute("src");
+    const modalContent = `
+      <img src="${imageUrl}" alt="">
+      <span class="close">&times;</span>
+    `;
+    modal.innerHTML = modalContent;
+    modal.style.display = "block";
+  }
+});
+
+// Fecha o lightbox quando o usuário clica no botão de fechar ou fora da imagem
+modal.addEventListener("click", (event) => {
+  if (event.target.classList.contains("close")) {
+    modal.style.display = "none";
+  }
+});
